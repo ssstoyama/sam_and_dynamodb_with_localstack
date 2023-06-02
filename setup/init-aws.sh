efe2ebb6d7e5496f3df533794dab81f1a8e45bc0
toyama.shoya+1-at-543179049845 <toyama.shoya@robon.co.jp>
2023-06-02 07:01:02 +0000

    fix

--- setup/init-aws.sh
+++ setup/init-aws.sh
@@ -8,16 +8,17 @@ export AWS_DEFAULT_REGION=ap-northeast-1
 # aws コマンドの向き先を Localstack に向ける
 aws="aws --endpoint-url http://localhost:4566"
 
-# setup 用ディレクトリに移動
+# このスクリプトのあるディレクトリに移動
 cd "$(dirname "$0")"
 
 # DynamoDB テーブル作成
 ${aws} dynamodb create-table --cli-input-json file://sample_table.json
-# 初期データ投入
+# テストデータ追加
 ${aws} dynamodb put-item --table-name sample --item '{"id":{"S":"id1"}}'
 ${aws} dynamodb put-item --table-name sample --item '{"id":{"S":"id2"}}'
 ${aws} dynamodb put-item --table-name sample --item '{"id":{"S":"id3"}}'
 
 # S3 バケット作成
 ${aws} s3api create-bucket --bucket sample-bucket --create-bucket-configuration LocationConstraint=ap-northeast-1
+# sample.txt 追加
 ${aws} s3api put-object --bucket sample-bucket --key sample.txt --body ./sample.txt
